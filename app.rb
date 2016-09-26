515bbfb58326e4da2a027e38227a8dc60797ed03
Jonas Demeulemeester <jonasdemeulemeester@hotmail.be>
2016-09-26 11:28:05 +0200

    Implement to_html (WIP)

--- app.rb
+++ app.rb
@@ -2,15 +2,26 @@ require './lib/temp_convert.rb'
 require 'net/http'
 require 'uri'
 
-#Command line input
-#input = ARGV.first.to_f
+selector = ARGV.first.to_f
 
-#Text file input
- #input = File.open('sensor.txt').read.to_f
+if selector == 1
+  #Command line input
+  input = ARGV[1].to_f
+end
 
-#URL input
-input = Net::HTTP.get(URI.parse("http://labict.be/software-engineering/temperature/api/temperature/fake")).to_f
+if selector == 2
+  #Text file input
+  input = File.open('sensor.txt').read.to_f
+end
+
+if selector == 3
+  #URL input
+  input = Net::HTTP.get(URI.parse("http://labict.be/software-engineering/temperature/api/temperature/fake")).to_f
+end
+
 
 temp_convert = TempConverter.new input
 
-puts temp_convert.convert
+#puts temp_convert.to_text
+#puts temp_convert.to_json
+puts temp_convert.to_html
--- lib/temp_convert.rb
+++ lib/temp_convert.rb
@@ -1,3 +1,4 @@
+require 'json'
 
 class TempConverter
 
@@ -5,10 +6,20 @@ class TempConverter
     @temperature = temperature
   end
 
-  def convert
-   puts "Gegeven: " + @temperature.to_s + " °C"
-   puts (@temperature).to_s + " °C = " + (@temperature * 9/5 + 32).to_s + " °F"
-   puts (@temperature).to_s + " °C = " + (@temperature + 273.15).to_s + " K"
+  def to_text
+    puts "Gegeven: " + @temperature.to_s + " °C"
+    puts (@temperature).to_s + " °C = " + (@temperature * 9/5 + 32).to_s + " °F"
+    puts (@temperature).to_s + " °C = " + (@temperature + 273.15).to_s + " K"
+  end
+
+  def to_json
+    json_temperature = {:Celsius => @temperature, :Fahrenheit => (@temperature * 9/5 + 32).to_s, :Kelvin => (@temperature + 273.15).to_s}
+    puts JSON.pretty_generate(json_temperature)
+  end
+
+  def to_html
+    html_temperature = <div><div>@temperature.to_s + " °C"</div><div>(@temperature * 9/5 + 32).to_s + " °F"</div><div>(@temperature + 273.15).to_s + " K"</div></div>
+    puts html_temperature.html_safe
   end
 
 end
