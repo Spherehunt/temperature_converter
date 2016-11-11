250e276757cf6b04d9016ffc95ff702ead30ee3b
Jonas Demeulemeester <jonasdemeulemeester@hotmail.be>
2016-11-11 17:31:21 +0100

    minor tweaks to commandline_reader WIP

--- lib/commandline_reader.rb
+++ lib/commandline_reader.rb
@@ -3,9 +3,9 @@ class CommandlineReader
 
   attr_accessor :temperature
 
-  # def initialize(temperature)
-  #   @temperature = temperature
-  # end
+  def initialize(temperature)
+    @temperature = temperature
+  end
 
   def self.read argument
     @temperature = argument.first.to_f
